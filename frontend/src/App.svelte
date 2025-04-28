bfb5a311614acd86ea2e1883bbd1173141554855
Loc <locnguyen110816@gmail.com>
2025-04-28 13:18:16 -0700

    Add HW1 to Svelte

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,64 +1,170 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import svelteLogo from './assets/svelte.svg';
-  import viteLogo from '/vite.svg';
-  import Counter from './lib/Counter.svelte';
+  import { onMount } from "svelte";
+  import nytLogo from "./assets/nyt-logo.png";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
 
-  let apiKey: string = '';
+  let apiKey: string = "";
 
   onMount(async () => {
     try {
-      const res = await fetch('/api/key');
+      const res = await fetch("/api/key");
       const data = await res.json();
       apiKey = data.apiKey;
     } catch (error) {
-      console.error('Failed to fetch API key:', error);
+      console.error("Failed to fetch API key:", error);
     }
   });
+
+  const date = new Date();
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  const date_display = date.toLocaleDateString("en-US", options);
 </script>
 
+<header>
+  <div class="header-container">
+    <!-- logo image and dynamic date -->
+    <img src={nytLogo} alt="The New York Times" />
+    <div id="current-date">{date_display}</div>
+  </div>
+  <nav class="main-nav">
+    <ul>
+      <!-- list of potential pages -->
+      <li class="nav-item"><a href="#">U.S.</a></li>
+      <li class="nav-item"><a href="#">World</a></li>
+      <li class="nav-item"><a href="#">Business</a></li>
+      <li class="nav-item"><a href="#">Arts</a></li>
+      <li class="nav-item"><a href="#">Lifestyle</a></li>
+      <li class="nav-item"><a href="#">Opinion</a></li>
+      <li class="nav-item"><a href="#">Audio</a></li>
+      <li class="nav-item"><a href="#">Games</a></li>
+      <li class="nav-item"><a href="#">Cooking</a></li>
+      <li class="nav-item"><a href="#">Wirecutter</a></li>
+      <li class="nav-item"><a href="#">Atlantic</a></li>
+    </ul>
+  </nav>
+</header>
+<hr />
+<hr />
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
+  <!-- main content area displayed as dynamic grid layout -->
+  <article>
+    <h2>Article 1</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipiscing elit. Amet consectetur
+      adipiscing elit quisque faucibus ex sapien. Quisque faucibus ex sapien
+      vitae pellentesque sem placerat. Vitae pellentesque sem placerat in id
+      cursus mi.
+    </p>
+    <a
+      title="Keith Weller/USDA, Public domain, via Wikimedia Commons"
+      href="https://commons.wikimedia.org/wiki/File:Cow_female_black_white.jpg"
+    >
+      <img
+        width="512"
+        alt="Black White cow in green grass field in farm (livestock)"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Cow_female_black_white.jpg/512px-Cow_female_black_white.jpg?20230829033145"
+      />
     </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Your API Key: <strong>{apiKey}</strong>
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    <figcaption>
+      <em>Keith Weller/USDA, Public domain, via Wikimedia Commons</em>
+    </figcaption>
+  </article>
+  <article>
+    <h2>Article 2</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet
+      consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque
+      faucibus ex sapien vitae pellentesque.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet
+      consectetur adipiscing elit quisque faucibus.
+    </p>
+  </article>
+  <article>
+    <h2>Article 3</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet
+      consectetur adipiscing elit quisque faucibus.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet
+      consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque
+      faucibus ex sapien vitae pellentesque.
+    </p>
+  </article>
+  <article>
+    <h2>Article 4</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet
+      consectetur adipiscing elit quisque faucibus.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet
+      consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque
+      faucibus ex sapien vitae pellentesque.
+    </p>
+    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+  </article>
+  <article>
+    <h2>Article 5</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+    <!-- image of HTML5 validator pass  -->
+    <img src="..static/images/valid-html.png" alt="Valid HTML5" />
+  </article>
+  <article>
+    <h2>Article 6</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet
+      consectetur adipiscing elit quisque faucibus.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet
+      consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque
+      faucibus ex sapien vitae pellentesque.
+    </p>
+  </article>
+  <article>
+    <h2>Article 7</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet
+      consectetur adipiscing elit quisque faucibus.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet
+      consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque
+      faucibus ex sapien vitae pellentesque.
+    </p>
+    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+  </article>
 </main>
+<footer>
+  <!-- footer for additonal information -->
+  <div class="footer-content">
+    <p>
+      <!-- CSS validator pass -->
+      <a href="https://jigsaw.w3.org/css-validator/check/referer">
+        <img
+          style="border:0;width:88px;height:31px"
+          src="https://jigsaw.w3.org/css-validator/images/vcss"
+          alt="Valid CSS!"
+        />
+      </a>
+    </p>
+    <p>&copy; 2024 The New York Times</p>
+    <p>Zoey Vo</p>
+    <p>Text generated with:</p>
+    <a href="https://loremipsum.io/generator/" target="_blank"
+      >Lorem Ipsum Generator</a
+    >
+  </div>
+</footer>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
